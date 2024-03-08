b68ba566675606401dd8fd76913c00648d0eafd6
Jorge <jorgebandeo@hotmail.com>
2024-03-08 12:47:06 -0300

    modelo de baner rotativo criado, falta redefinição movaile

--- js/Evento.js
+++ js/Evento.js
@@ -32,17 +32,26 @@ window.addEventListener('resize', function() {
     }
 });
 
-
+let idx = 0;
 var imgelement;
+var divition_img;
 
 // Definir uma função para atualizar os dados
 function atualizarDados() {
-    // Coloque aqui o código para atualizar os dados conforme necessário
-    for (let index = 0; index < imgelement.length; index++) {
-        imgelement[index].style.width = ((window.innerWidth/3) -10) +"px" ;
-        imgelement[index].style.height = "auto";
+    if (window.innerWidth <= 600) {
+        divition_img = ((window.innerWidth/2)) ;
+    }else{
+        divition_img = ((window.innerWidth/3))  ;
     }
 
+    // Coloque aqui o código para atualizar os dados conforme necessário
+    for (let index = 0; index < imgelement.length; index++) {
+        imgelement[index].style.width = (divition_img -10) +"px" ;
+        imgelement[index].style.height = "auto";
+    }
+    idx = 0;
+    const imgs = document.getElementById("img");
+    imgs.style.transform = `translateX(${0}px)`;
 }
 
 // Adicionar um ouvinte de evento para o redimensionamento da tela
@@ -76,6 +85,14 @@ async function start() {
         });
 
         request.execute(function(resp) {
+
+            if (window.innerWidth <= 600) {
+                divition_img = ((window.innerWidth/2)) ;
+            }else{
+                divition_img = ((window.innerWidth/3))  ;
+            }
+
+
             var files = resp.files;
             if (files && files.length > 0) {
                 console.log('Arquivos:');
@@ -88,7 +105,8 @@ async function start() {
                         img.src = 'https://drive.google.com/thumbnail?id=' + file.id + '&sz=w1000';
                         img.alt = file.name;
                         img.id = "aaaaa";
-                        img.style.width = ((window.innerWidth/3) -10) +"px" ;
+                        img.style.width = (divition_img - 10 )+"px";
+
                         img.style.marginLeft = "5px";
                         img.style.marginBottom = "10px"
                         img.style.marginRight = "5px";
@@ -106,7 +124,7 @@ async function start() {
             imgelement = imgs.querySelectorAll("*");
 
             
-            let idx = 0;
+            
         
             function carrossel() {
                 idx++;
@@ -115,7 +133,7 @@ async function start() {
                     idx = 0;
                 }
                 if (imgs) {
-                    imgs.style.transform = `translateX(${-idx * ((window.innerWidth/3) )}px)`;
+                    imgs.style.transform = `translateX(${-idx * (divition_img  )}px)`;
                 }
             }
         
